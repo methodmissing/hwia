06fbeb97c6cdb5d53cee6581337c604d6d5b50aa
Lourens Naudé <lourens@methodmissing.com>
2009-08-19 23:54:09 +0100

    Win.API compat with core Hash + minor test failures to fix

--- ext/hwia/hwia.c
+++ ext/hwia/hwia.c
@@ -1,4 +1,13 @@
 #include "ruby.h"
+#ifndef RUBY18
+#include "ruby/st.h"
+#elif
+#include "st.h"
+#endif
+
+VALUE rb_cHashWithIndifferentAccess;
+
+static ID id_hwia_hash, id_hash;
 
 #ifndef RSTRING_PTR
 #define RSTRING_PTR(obj) RSTRING(obj)->ptr
@@ -18,28 +27,209 @@ static int strhash(register const char *string)
     return val + (val>>5);
 }
 
-static VALUE
-rb_sym_hwia_hash(sym)
-    VALUE sym;
+int
+rb_sym_hwia_hash(VALUE sym)
 {
-	char *name;
 	ID id = SYM2ID(sym);
-	name = rb_id2name(id);
-    return INT2FIX(strhash(name));
+    return strhash((char*)rb_id2name(id));
 }
 
 static VALUE
-rb_str_hwia_hash(str)
-    VALUE str;
+rb_sym_hwia_hash_m(VALUE sym)
 {
-	char *name;
-	name = RSTRING_PTR(str);
-    return INT2FIX(strhash(name));
+    return INT2FIX(rb_sym_hwia_hash(sym));
+}
+
+int
+rb_str_hwia_hash(VALUE str)
+{
+    return strhash((char*)RSTRING_PTR(str));
+}
+
+static VALUE
+rb_str_hwia_hash_m(VALUE str)
+{
+    return INT2FIX(rb_str_hwia_hash(str));
+}
+
+int
+rb_hwia_cmp(VALUE s1,VALUE s2)
+{
+	int s1_hash = SYMBOL_P(s1) ? rb_sym_hwia_hash(s1) : rb_str_hwia_hash(s1);
+	int s2_hash = SYMBOL_P(s2) ? rb_sym_hwia_hash(s2) : rb_str_hwia_hash(s2);
+	if (s1_hash == s2_hash) return 0;
+	if (s1_hash > s2_hash) return 1;
+	return -1;	
+}
+
+static VALUE
+eql(args)
+    VALUE *args;
+{
+    return (VALUE)rb_eql(args[0], args[1]);
+}
+
+static int
+rb_hwia_hash_cmp(a, b)
+    VALUE a, b;
+{
+    VALUE args[2];
+
+    if (a == b) return 0;
+    if (FIXNUM_P(a) && FIXNUM_P(b)) {
+	return a != b;
+    }
+    if (TYPE(a) == T_STRING && RBASIC(a)->klass == rb_cString &&
+	TYPE(b) == T_STRING && RBASIC(b)->klass == rb_cString) {
+	return rb_str_cmp(a, b);
+    }
+    if ((TYPE(a) == T_STRING && RBASIC(a)->klass == rb_cString && SYMBOL_P(b)) || (TYPE(b) == T_STRING && RBASIC(b)->klass == rb_cString && SYMBOL_P(a))) {
+	return rb_hwia_cmp(a, b);
+    }
+    if (a == Qundef || b == Qundef) return -1;
+    if (SYMBOL_P(a) && SYMBOL_P(b)) {
+	return a != b;
+    }
+
+    args[0] = a;
+    args[1] = b;
+    return !rb_with_disable_interrupt(eql, (VALUE)args);
+}
+
+VALUE
+rb_hwia(obj)
+    VALUE obj;
+{
+    return rb_funcall(obj, id_hash, 0);
+}
+
+static int
+rb_hwia_hash(a)
+    VALUE a;
+{
+    VALUE hval;
+
+    switch (TYPE(a)) {
+      case T_FIXNUM:
+	return (int)a;    
+      case T_SYMBOL:
+	return rb_sym_hwia_hash(a);
+	break;
+
+      case T_STRING:
+	return rb_str_hwia_hash(a);
+	break;
+
+      default:
+	hval = rb_funcall(a, id_hash, 0);
+	if (!FIXNUM_P(hval)) {
+	    hval = rb_funcall(hval, '%', 1, INT2FIX(536870923));
+	}
+	return (int)FIX2LONG(hval);
+    }
+}
+
+static struct st_hash_type objhwia = {
+    rb_hwia_hash_cmp,
+    rb_hwia_hash,
+};
+
+static VALUE hwia_alloc0 _((VALUE));
+static VALUE hwia_alloc _((VALUE));
+static VALUE
+hwia_alloc0(klass)
+    VALUE klass;
+{
+    NEWOBJ(hash, struct RHash);
+    OBJSETUP(hash, klass, T_HASH);
+
+    hash->ifnone = Qnil;
+
+    return (VALUE)hash;
+}
+
+static VALUE
+hwia_alloc(klass)
+    VALUE klass;
+{
+    VALUE hash = hwia_alloc0(klass);
+
+    RHASH(hash)->tbl = st_init_table(&objhwia);
+
+    return hash;
+}
+
+VALUE
+rb_hwia_new()
+{
+    return hwia_alloc(rb_cHashWithIndifferentAccess);
+}
+
+static VALUE
+rb_hwia_s_create(argc, argv, klass)
+    int argc;
+    VALUE *argv;
+    VALUE klass;
+{
+    VALUE hash;
+    int i;
+
+    if (argc == 1 && TYPE(argv[0]) == T_HASH) {
+	hash = hwia_alloc0(klass);
+	RHASH(hash)->tbl = st_copy(RHASH(argv[0])->tbl);
+	RHASH(hash)->tbl->type = &objhwia;
+	return hash;
+    }
+
+    if (argc % 2 != 0) {
+	rb_raise(rb_eArgError, "odd number of arguments for Hash");
+    }
+
+    hash = hwia_alloc(klass);
+    for (i=0; i<argc; i+=2) {
+        rb_hash_aset(hash, argv[i], argv[i + 1]);
+    }
+
+    return hash;
+}
+
+static int
+rb_hash_rehash_i(key, value, tbl)
+    VALUE key, value;
+    st_table *tbl;
+{
+    if (key != Qundef) st_insert(tbl, key, value);
+    return ST_CONTINUE;
+}
+
+static VALUE
+rb_hwia_rehash(hash)
+    VALUE hash;
+{
+    st_table *tbl;
+
+    rb_hash_modify(hash);
+    tbl = st_init_table_with_size(&objhwia, RHASH(hash)->tbl->num_entries);
+    rb_hash_foreach(hash, rb_hash_rehash_i, (st_data_t)tbl);
+    st_free_table(RHASH(hash)->tbl);
+    RHASH(hash)->tbl = tbl;
+
+    return hash;
 }
 
 void
 Init_hwia()
 {
-	rb_define_method(rb_cString, "hwia_hash", rb_str_hwia_hash, 0);
-	rb_define_method(rb_cSymbol, "hwia_hash", rb_sym_hwia_hash, 0);
+	id_hash = rb_intern("hash");
+	id_hwia_hash = rb_intern("hwia_hash");	
+	
+	rb_cHashWithIndifferentAccess = rb_define_class("HashWithIndifferentAccess", rb_cHash);
+
+	rb_undef_alloc_func(rb_cHashWithIndifferentAccess);
+    rb_define_alloc_func(rb_cHashWithIndifferentAccess, hwia_alloc);
+    rb_define_singleton_method(rb_cHashWithIndifferentAccess, "[]", rb_hwia_s_create, -1);
+	
+	rb_define_method(rb_cString, "hwia_hash", rb_str_hwia_hash_m, 0);
+	rb_define_method(rb_cSymbol, "hwia_hash", rb_sym_hwia_hash_m, 0);
+    rb_define_method(rb_cHashWithIndifferentAccess,"rehash", rb_hwia_rehash, 0);	
 }	
--- test/test_hwia.rb
+++ test/test_hwia.rb
@@ -13,3 +13,206 @@ class TestSymbolAndStringHash < Test::Unit::TestCase
     assert_equal str.hwia_hash, sym.hwia_hash
   end  
 end
+
+class TestHashWithIndifferentAccess < Test::Unit::TestCase
+  def setup
+    @strings = HashWithIndifferentAccess[ 'a' => 1, 'b' => 2 ]
+    @symbols = HashWithIndifferentAccess[ :a  => 1, :b  => 2 ]
+    @mixed   = HashWithIndifferentAccess[ :a  => 1, 'b' => 2 ]
+    @fixnums = HashWithIndifferentAccess[  0  => 1,  1  => 2 ]
+  end
+  
+  def test_inherits_hash
+    assert_equal Hash, HashWithIndifferentAccess.superclass
+  end  
+  
+  def test_keys
+    assert_equal ["a", "b"], @strings.keys
+    assert_equal [:a, :b], @symbols.keys
+    assert_equal [:a, "b"], @mixed.keys
+    assert_equal [0, 1], @fixnums.keys            
+  end
+  
+  def test_values
+    assert_equal [1, 2], @strings.values
+    assert_equal [1, 2], @symbols.values
+    assert_equal [1, 2], @mixed.values
+    assert_equal [1, 2], @fixnums.values         
+  end  
+ 
+  def test_fetch
+    assert_equal 1, @strings.fetch('a')
+    assert_equal 1, @strings.fetch(:a.to_s)
+    assert_equal 1, @strings.fetch(:a)
+  end
+  
+  def test_assorted
+    hashes = { :@strings => @strings, :@symbols => @symbols, :@mixed => @mixed }
+    method_map = { :'[]' => 1, :fetch => 1, :values_at => [1],
+      :has_key? => true, :include? => true, :key? => true,
+      :member? => true }
+
+    hashes.each do |name, hash|
+      method_map.sort_by { |m| m.to_s }.each do |meth, expected|
+        assert_equal(expected, hash.__send__(meth, 'a'),
+                     "Calling #{name}.#{meth} 'a'")
+        assert_equal(expected, hash.__send__(meth, :a),
+                     "Calling #{name}.#{meth} :a")
+      end
+    end
+
+    assert_equal [1, 2], @strings.values_at('a', 'b')
+    assert_equal [1, 2], @strings.values_at(:a, :b)
+    assert_equal [1, 2], @symbols.values_at('a', 'b')
+    assert_equal [1, 2], @symbols.values_at(:a, :b)
+    assert_equal [1, 2], @mixed.values_at('a', 'b')
+    assert_equal [1, 2], @mixed.values_at(:a, :b)
+  end
+  
+  def test_reading
+    hash = HashWithIndifferentAccess.new
+    hash["a"] = 1
+    hash["b"] = true
+    hash["c"] = false
+    hash["d"] = nil
+
+    assert_equal 1, hash[:a]
+    assert_equal true, hash[:b]
+    assert_equal false, hash[:c]
+    assert_equal nil, hash[:d]
+    assert_equal nil, hash[:e]
+  end  
+  
+  def test_reading_with_nonnil_default
+    hash = HashWithIndifferentAccess.new(1)
+    hash["a"] = 1
+    hash["b"] = true
+    hash["c"] = false
+    hash["d"] = nil
+
+    assert_equal 1, hash[:a]
+    assert_equal true, hash[:b]
+    assert_equal false, hash[:c]
+    assert_equal nil, hash[:d]
+    assert_equal 1, hash[:e]
+  end  
+
+  def test_writing
+    hash = HashWithIndifferentAccess.new
+    hash[:a] = 1
+    hash['b'] = 2
+    hash[3] = 3
+
+    assert_equal hash['a'], 1
+    assert_equal hash['b'], 2
+    assert_equal hash[:a], 1
+    assert_equal hash[:b], 2
+    assert_equal hash[3], 3
+  end  
+
+  def test_update
+    hash = HashWithIndifferentAccess.new
+    hash[:a] = 'a'
+    hash['b'] = 'b'
+
+    updated_with_strings = hash.update(@strings)
+    updated_with_symbols = hash.update(@symbols)
+    updated_with_mixed = hash.update(@mixed)
+
+    assert_equal updated_with_strings[:a], 1
+    assert_equal updated_with_strings['a'], 1
+    assert_equal updated_with_strings['b'], 2
+
+    assert_equal updated_with_symbols[:a], 1
+    assert_equal updated_with_symbols['b'], 2
+    assert_equal updated_with_symbols[:b], 2
+
+    assert_equal updated_with_mixed[:a], 1
+    assert_equal updated_with_mixed['b'], 2
+
+    assert [updated_with_strings, updated_with_symbols, updated_with_mixed].all? { |h| h.keys.size == 2 }
+  end  
+  
+  def test_merging
+    hash = HashWithIndifferentAccess.new
+    hash[:a] = 'failure'
+    hash['b'] = 'failure'
+
+    other = { 'a' => 1, :b => 2 }
+
+    merged = hash.merge(other)
+
+    assert_equal HashWithIndifferentAccess, merged.class
+    assert_equal 1, merged[:a]
+    assert_equal 2, merged['b']
+
+    hash.update(other)
+
+    assert_equal 1, hash[:a]
+    assert_equal 2, hash['b']
+  end  
+  
+  def test_deleting
+    get_hash = proc{ HashWithIndifferentAccess[ :a => 'foo' ] }
+    hash = get_hash.call
+    assert_equal hash.delete(:a), 'foo'
+    assert_equal hash.delete(:a), nil
+    hash = get_hash.call
+    assert_equal hash.delete('a'), 'foo'
+    assert_equal hash.delete('a'), nil
+  end  
+
+=begin  
+  def test_hash_with_array_of_hashes
+    hash = { "urls" => { "url" => [ { "address" => "1" }, { "address" => "2" } ] }}
+    hwia = HashWithIndifferentAccess[hash]
+    assert_equal "1", hwia[:urls][:url].first[:address]
+  end
+  
+  def test_indifferent_subhashes
+    h = {'user' => {'id' => 5}}.with_indifferent_access
+    ['user', :user].each {|user| [:id, 'id'].each {|id| assert_equal 5, h[user][id], "h[#{user.inspect}][#{id.inspect}] should be 5"}}
+
+    h = {:user => {:id => 5}}.with_indifferent_access
+    ['user', :user].each {|user| [:id, 'id'].each {|id| assert_equal 5, h[user][id], "h[#{user.inspect}][#{id.inspect}] should be 5"}}
+  end
+  
+  def test_indifferent_slice
+    original = { :a => 'x', :b => 'y', :c => 10 }.with_indifferent_access
+    expected = { :a => 'x', :b => 'y' }.with_indifferent_access
+
+    [['a', 'b'], [:a, :b]].each do |keys|
+      # Should return a new hash with only the given keys.
+      assert_equal expected, original.slice(*keys), keys.inspect
+      assert_not_equal expected, original
+    end
+  end
+
+  def test_indifferent_slice_inplace
+    original = { :a => 'x', :b => 'y', :c => 10 }.with_indifferent_access
+    expected = { :c => 10 }.with_indifferent_access
+
+    [['a', 'b'], [:a, :b]].each do |keys|
+      # Should replace the hash with only the given keys.
+      copy = original.dup
+      assert_equal expected, copy.slice!(*keys)
+    end
+  end
+
+  def test_indifferent_slice_access_with_symbols
+    original = {'login' => 'bender', 'password' => 'shiny', 'stuff' => 'foo'}
+    original = original.with_indifferent_access
+
+    slice = original.slice(:login, :password)
+
+    assert_equal 'bender', slice[:login]
+    assert_equal 'bender', slice['login']
+  end
+  
+  def test_should_copy_the_default_value_when_converting_to_hash_with_indifferent_access
+    hash = Hash.new(3)
+    hash_wia = hash.with_indifferent_access
+    assert_equal 3, hash_wia.default
+  end    
+=end    
+end
